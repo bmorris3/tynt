408fa6122e97e876d1f50baaa49e885cb4dc2454
Brett Morris <brettmorris21@gmail.com>
2019-06-11 20:07:15 -0700

    Adding first tests

--- tynt/core.py
+++ tynt/core.py
@@ -1,30 +1,83 @@
 import numpy as np
 from astropy.io import fits
 from astropy.table import Table
+from astropy.utils.data import download_file
 import astropy.units as u
 import os
 
-__all__ = ['FourierFilter']
+__all__ = ['Filter']
 
 data_path = os.path.join(os.path.dirname(__file__), 'data', 'fft.fits')
 
-class FourierFilter(object):
-    def __init__(self, path=data_path): 
+
+class Filter(object):
+    def __init__(self, path=data_path):
+        """
+        Parameters
+        ----------
+        path : str (optional)
+            Path to `fft.fits` file
+        """
         self.path = path
-        self.bintable = fits.getdata(path)
+        self.table = Table(fits.getdata(path))
+        self.table.add_index('col0')
         
     def available_filters(self):
-        return self.bintable.names
+        """
+        Return the available filters in the archive
+        """
+        return self.table['col0'].data
         
     def reconstruct(self, identifier):
-        filt = self.bintable[identifier]
-        n_lambda, lambda_0, delta_lambda, tr_max = filt[:4].real
+        """
+        Reconstruct an approximate filter transmittance curve for
+        a given filter. 
+        
+        Parameters
+        ----------
+        identifier : str
+            Name of the filter. To see available filters, run `~tynt.Filter.available_filters()`
+        
+        Returns
+        -------
+        wavelength : `~numpy.ndarray`
+            Wavelength array
+        transmittance : `~numpy.ndarray`
+            Approximate transmittance as a function of wavelength
+        """
+        filt = list(self.table.loc[identifier])[1:]
+        n_lambda, lambda_0, delta_lambda, tr_max = filt[:4]
         fft = filt[4:]
 
-        wavelength = np.arange(lambda_0, (n_lambda + 1) * delta_lambda + lambda_0, delta_lambda)
+        wavelength = np.arange(lambda_0, (n_lambda + 1) * delta_lambda + lambda_0, 
+                               delta_lambda)
 
         ifft = np.fft.ifft(fft, n=len(wavelength))
 
-        flux = (ifft.real - ifft.real.min()) * tr_max / ifft.real.ptp()
+        transmittance = (ifft.real - ifft.real.min()) * tr_max / ifft.real.ptp()
 
-        return wavelength, flux
+        return wavelength, transmittance
+
+    def download_true_transmittance(self, identifier): 
+        """
+        Query the SVO service for a given filter, return the true transmittance
+        curve. 
+        
+        Parameters
+        ----------
+        identifier : str
+            Name of the filter. To see available filters, run `~tynt.Filter.available_filters()`
+        
+        Returns
+        -------
+        wavelength : `~numpy.ndarray`
+            True wavelength array
+        transmittance : `~numpy.ndarray`
+            True transmittance as a function of wavelength        
+        """
+        path = download_file('http://svo2.cab.inta-csic.es/theory/fps3/fps.php?ID={0}'
+                             .format(identifier))
+        
+        true_transmittance = Table.read(path, format='votable')
+        return (true_transmittance['Wavelength'].data.data, 
+                true_transmittance['Transmission'].data.data)
---
+++ tynt/tests/test_core.py
@@ -0,0 +1,39 @@
+from astropy.tests.helper import remote_data
+import numpy as np
+
+from ..core import Filter
+
+f = Filter()
+
+@remote_data
+def test_sdss_r(): 
+
+    filt = 'SLOAN/SDSS.rprime_filter'
+
+    approx_wl, approx_tr = f.reconstruct(filt)
+    true_wl, true_tr = f.download_true_transmittance(filt)
+
+    np.testing.assert_allclose(np.interp(true_wl, approx_wl, approx_tr),
+                               true_tr, atol=0.5)
+
+@remote_data
+def test_sdss_g():
+
+    filt = 'SLOAN/SDSS.gprime_filter'
+
+    approx_wl, approx_tr = f.reconstruct(filt)
+    true_wl, true_tr = f.download_true_transmittance(filt)
+
+    np.testing.assert_allclose(np.interp(true_wl, approx_wl, approx_tr),
+                               true_tr, atol=0.07)
+
+@remote_data
+def test_sdss_i():
+
+    filt = 'SLOAN/SDSS.iprime_filter'
+
+    approx_wl, approx_tr = f.reconstruct(filt)
+    true_wl, true_tr = f.download_true_transmittance(filt)
+
+    np.testing.assert_allclose(np.interp(true_wl, approx_wl, approx_tr),
+                               true_tr, atol=0.05)
