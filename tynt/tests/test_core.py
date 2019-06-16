87c9c23c68d5d47df7c28c6f90fa8e2b4c49b549
Brett Morris <brettmorris21@gmail.com>
2019-06-16 14:52:12 -0700

    Adding basic sdss offline tests

--- tynt/tests/test_core.py
+++ tynt/tests/test_core.py
@@ -1,5 +1,6 @@
-from astropy.tests.helper import remote_data
 import numpy as np
+from astropy.tests.helper import remote_data, assert_quantity_allclose
+import astropy.units as u
 
 from ..core import FilterGenerator
 
@@ -7,7 +8,7 @@ f = FilterGenerator()
 
 
 @remote_data
-def test_sdss_r():
+def test_download_sdss_r():
 
     identifier = 'SLOAN/SDSS.rprime_filter'
 
@@ -21,7 +22,7 @@ def test_sdss_r():
 
 
 @remote_data
-def test_sdss_g():
+def test_download_sdss_g():
 
     identifier = 'SLOAN/SDSS.gprime_filter'
 
@@ -35,7 +36,7 @@ def test_sdss_g():
 
 
 @remote_data
-def test_sdss_i():
+def test_download_sdss_i():
 
     identifier = 'SLOAN/SDSS.iprime_filter'
 
@@ -46,3 +47,37 @@ def test_sdss_i():
 
     np.testing.assert_allclose(np.interp(true_wl, approx_wl, approx_tr),
                                true_tr, atol=0.05)
+
+
+def test_sdss_lambda_eff_w_eff():
+
+    sdss_filters = ['SLOAN/SDSS.u', 'SLOAN/SDSS.g',
+                    'SLOAN/SDSS.r', 'SLOAN/SDSS.i',
+                    'SLOAN/SDSS.z']
+
+    # Answers from:
+    # http://svo2.cab.inta-csic.es/theory/fps/index.php?mode=browse&gname=SLOAN
+    lambda_mean_true = u.Quantity([3561.8, 4718.9, 6185.2, 7499.7, 8961.5],
+                                  u.Angstrom)
+
+    w_eff_true = u.Quantity([558.4, 1158.4, 1111.2, 1044.6, 1124.6],
+                             u.Angstrom)
+    w_eff_approx = []
+    lambda_mean_approx = []
+
+    for identifier in sdss_filters:
+        filt = f.reconstruct(identifier)
+
+        lambda_bar_approx = (np.trapz(filt.transmittance * filt.wavelength,
+                                      filt.wavelength) /
+                             np.trapz(filt.transmittance, filt.wavelength))
+
+        width_approx = (np.trapz(filt.transmittance, filt.wavelength) /
+                        filt.transmittance.max())
+
+        w_eff_approx.append(width_approx)
+        lambda_mean_approx.append(lambda_bar_approx)
+
+    assert_quantity_allclose(w_eff_approx, w_eff_true,
+                             rtol=0.05)
+    assert_quantity_allclose(lambda_mean_approx, lambda_mean_true, rtol=0.01)
